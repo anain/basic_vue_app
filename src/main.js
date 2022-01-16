f5c4c58f1b7233a3189336f566e3576c7e6c478c
anain <anain@student,42,fr>
2022-01-16 22:03:33 +0100

    Initialize db

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8aI159wnOAzB6e8X9ubVQWlUGps7pyEs",
+  authDomain: "hello-d9243.firebaseapp.com",
+  projectId: "hello-d9243",
+  storageBucket: "hello-d9243.appspot.com",
+  messagingSenderId: "1090585560741",
+  appId: "1:1090585560741:web:2f11ccd541b09213d2af4c"
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
