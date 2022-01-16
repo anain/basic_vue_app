8d024e1742f2bde11ff26e331afa495a39971ea5
anain <anain@student,42,fr>
2022-01-16 22:04:02 +0100

    Create welcome form

---
+++ src/views/Welcome.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="welcome">
+    <form
+        id="user-identification"
+        @submit.prevent="saveName"
+    >
+      <p>Bonjour ! Qui êtes-vous ?</p>
+      <input v-model.trim="username"> 
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+      return {
+      username: 'nom'
+    }
+  },
+  methods : {
+    saveName() {
+      alert('Bonjour' + this.username)
+      this.$router.push('records')
+    }
+  }
+}
+
+</script>
+
+<style>
+
+#welcome {
+  font-size: 25px;
+}
+
+input { 
+  padding: 5px 5px;
+  font-size: 25px;
+}
+
+</style>
